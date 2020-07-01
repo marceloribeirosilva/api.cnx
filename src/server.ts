3da6a89d5498c31f64a3f5bf1329d1d03707444d
marceloribeirosilva <ribmarc@gmail.com>
2020-07-01 13:59:35 -0300

    refactor: adjusts server to appError

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
 
 import routes from './routes';
 
@@ -11,6 +15,18 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
