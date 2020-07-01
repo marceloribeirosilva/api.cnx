85725e9acf90d17bf69bccf504b15cf19989ca6a
marceloribeirosilva <ribmarc@gmail.com>
2020-07-01 14:15:11 -0300

    feat: create user session authenticated with jwt

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import AuthenticateUseService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  const { email, password } = request.body;
+
+  const authenticateUser = new AuthenticateUseService();
+
+  const { user, token } = await authenticateUser.execute({ email, password });
+
+  delete user.password;
+
+  return response.json({ user, token });
+});
+
+export default sessionsRouter;
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
+
+import authConfig from '../config/auth';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+class AutheticateUserService {
+  public async execute({
+    email,
+    password,
+  }: Request): Promise<{ user: User; token: string }> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new AppError('E-mail ou senha estão incorretas', 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('E-mail ou senha estão incorretas', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AutheticateUserService;
