4e84890be45113e98dac0c2cdf4c855e02a9a22e
marceloribeirosilva <ribmarc@gmail.com>
2020-07-03 11:40:15 -0300

    feat: add controllers and routes password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
-import PlaylistsRouter from '@modules/playlists/infra/http/routes/playlists.routes';
+import playlistsRouter from '@modules/playlists/infra/http/routes/playlists.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-routes.use('/playlists', PlaylistsRouter);
+routes.use('/playlists', playlistsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
